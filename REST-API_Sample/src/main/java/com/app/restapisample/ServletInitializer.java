f8d32e33f4c7e8ddc45d845295127732e975ecdb
egnsuresh <39545931+egnsuresh@users.noreply.github.com>
2020-02-27 12:31:15 +0530

    Update and rename REST-API_Sample/src/main/java/com/app/releasenotes/ServletInitializer.java to REST-API_Sample/src/main/java/com/app/restapisample/ServletInitializer.java

--- REST-API_Sample/src/main/java/com/app/restapisample/ServletInitializer.java
+++ REST-API_Sample/src/main/java/com/app/restapisample/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.app.releasenotes;
+package com.app.restapisample;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
