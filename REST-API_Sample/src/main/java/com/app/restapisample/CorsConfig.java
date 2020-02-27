1f7fa59b71c0ce5d1d6abe26677f267c9da4db70
egnsuresh <39545931+egnsuresh@users.noreply.github.com>
2020-02-27 12:30:30 +0530

    Update and rename REST-API_Sample/src/main/java/com/app/releasenotes/CorsConfig.java to REST-API_Sample/src/main/java/com/app/restapisample/CorsConfig.java

--- REST-API_Sample/src/main/java/com/app/restapisample/CorsConfig.java
+++ REST-API_Sample/src/main/java/com/app/restapisample/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.app.releasenotes;
+package com.app.restapisample;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
